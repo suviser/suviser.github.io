8388b0ceb38986e8fe4a8375a86b063e944150f6
suviser <suviser427@gmail.com>
2023-12-09 16:11:23 +0200

    обновил аватарки, добавил кнопки
    добавил возможность переключать секторы, таким образом каждый вариант выберет себе нужное.

---
+++ main.js
@@ -0,0 +1,12 @@
+let currentSectionId = 'section1';
+
+function toggleSection(sectionId) {
+    // Скрываем только текущую видимую секцию
+    document.getElementById(currentSectionId).classList.add('hidden');
+
+    // Показываем выбранную секцию
+    document.getElementById(sectionId).classList.remove('hidden');
+
+    // Обновляем текущую видимую секцию
+    currentSectionId = sectionId;
+}
