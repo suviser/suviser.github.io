ccfa017ac0a5582bef20210779975fb651e838e4
suviser <suviser427@gmail.com>
2024-05-25 14:51:42 +0300

    fsfs

---
+++ footer.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    fetch('footer.html')
+      .then(response => response.text())
+      .then(text => document.getElementById('footer-placeholder').innerHTML = text);
+  });
