8a83b8e493b3f79bdc826609c752086a787d24d2
suviser <suviser427@gmail.com>
2024-06-08 23:19:37 +0300

    Обновил стили кнопок, добавил больше рабочих

--- team.js
+++ team.js
@@ -1,14 +1,24 @@
-let currentSectionId = 'section1';
+// let currentSectionId = 'section0';
+
+// function toggleSection(sectionId) {
+//     // Скрываем только текущую видимую секцию
+//     document.getElementById(currentSectionId).classList.add('hidden');
+
+//     // Показываем выбранную секцию
+//     document.getElementById(sectionId).classList.remove('hidden');
+
+//     // Обновляем текущую видимую секцию
+//     currentSectionId = sectionId;
+// }
+
 
 function toggleSection(sectionId) {
-    // Скрываем только текущую видимую секцию
-    document.getElementById(currentSectionId).classList.add('hidden');
-
-    // Показываем выбранную секцию
-    document.getElementById(sectionId).classList.remove('hidden');
-
-    // Обновляем текущую видимую секцию
-    currentSectionId = sectionId;
+    const sections = document.querySelectorAll('section[id^="section"]');
+    sections.forEach(section => {
+        section.classList.remove('active');
+        section.classList.add('hidden');
+    });
+    const activeSection = document.getElementById(sectionId);
+    activeSection.classList.remove('hidden');
+    activeSection.classList.add('active');
 }
-
-
