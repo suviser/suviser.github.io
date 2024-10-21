f53b974f8cfba43cafdff4e2c1501f693fec579f
suviser <suviser427@gmail.com>
2024-10-21 18:54:54 +0200

    Добаваил английский

---
+++ navigation_en.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    fetch('navigation_en.html')
+      .then(response => response.text())
+      .then(text => document.getElementById('nav-placeholder').innerHTML = text);
+  });
